0350381c3deffe001f45a6e51799d2da13da8ad3
JiHwan Park <121422879+ParkJiHwan22@users.noreply.github.com>
2023-01-22 10:45:26 +0900

    [Silver IV] Title: 대칭 차집합, Time: 260 ms, Memory: 80912 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,23 +1,5 @@
 A, B = map(int, input().split())
-A_list = list(map(int, input().split()))
-B_list = list(map(int, input().split()))
-A_dict = {}
-B_dict = {}
+A_list = set(map(int, input().split()))
+B_list = set(map(int, input().split()))
 
-for i in A_list:
-    A_dict[i] = 0
-
-for i in B_list:
-    B_dict[i] = 0
-
-cnt_A = 0
-for i in A_list:
-    if i not in B_dict:
-        cnt_A += 1
-
-cnt_B = 0
-for i in B_list:
-    if i not in A_dict:
-        cnt_B += 1
-
-print(cnt_A + cnt_B)
+print(len(A_list-B_list)+len(B_list-A_list))
