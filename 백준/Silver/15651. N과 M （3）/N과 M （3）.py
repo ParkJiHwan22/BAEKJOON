a02b38478ed8c18701661ebc974b88c0b149cbfc
JiHwan Park <121422879+ParkJiHwan22@users.noreply.github.com>
2023-02-01 18:37:52 +0900

    [Silver III] Title: N과 M (3), Time: 1376 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,12 +1,14 @@
-from itertools import product
-
-n, m = map(int, input().split())
-num = [i for i in range(1, n+1)]
-
-answer = list(product(num, repeat = m))
-
-for x in answer:
-    num_list = []
-    for y in x:
-        num_list.append(y)
-    print(*num_list, sep = ' ')
+n, m= map(int,input().split())
+ 
+s = []
+ 
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, n+1):
+        s.append(i)
+        dfs()
+        s.pop()
+dfs()
