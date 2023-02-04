aef1e50dadedf3283bedba50fe49ccfad7e50333
JiHwan Park <121422879+ParkJiHwan22@users.noreply.github.com>
2023-02-04 11:16:31 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 156 ms, Memory: 36916 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,24 +1,10 @@
-T = int(input())
-for _ in range(T):
-    n, m = map(int, input().split())
-    eat = sorted(list(map(int, input().split())))
-    eaten = sorted(list(map(int, input().split())))
+import bisect
 
+for _ in range(int(input())):
+    N, M = map(int, input().split())
+    A = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split())))
     cnt = 0
-    for x in eat:
-        start, end = 0, m-1
-        k = -1
-
-        while start <= end:
-            mid = (start + end) // 2
-
-            if x <= eaten[mid]:
-                end = mid - 1
-
-            elif x > eaten[mid]:
-                k = mid
-                start = mid + 1
-        
-        if k >= 0:
-            cnt += k + 1 
+    for a in A:
+        cnt += (bisect.bisect(B, a-1))
     print(cnt)
