0d74466cd809dae540f077827223775105ee3853
JiHwan Park <bat522@naver.com>
2023-02-13 00:01:01 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,8 +1,8 @@
 n, k = map(int, input().split())
 coin = [int(input()) for _ in range(n)]
-
+coin = sorted(coin, reverse = True)
 res = 0
-for i in range(n - 1, -1, -1):
+for i in range(n):
     if k >= coin[i]:
         res += k // coin[i]
         k = k % coin[i]
