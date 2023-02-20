b681650680daec15490bdb373a4af2294b14fee0
JiHwan Park <bat522@naver.com>
2023-02-20 19:25:15 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 36716 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,12 +1,8 @@
 from itertools import permutations
 
 n, m = map(int, input().split())
-num_list = []
+num_list = [i for i in range(1, n+1)]
+combi = list(permutations(num_list, m))
 
-for i in range(1, n+1):
-    num_list.append(i)
-
-permu = list(permutations(num_list,m))
-
-for i in range(len(permu)):
-    print(*permu[i])
+for x in range(len(combi)):
+    print(*combi[x])
