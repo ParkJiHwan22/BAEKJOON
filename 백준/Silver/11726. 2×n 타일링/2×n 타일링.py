2bfa2e6115064067961c92806fa512d03b64f101
JiHwan Park <bat522@naver.com>
2023-03-05 01:20:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,17 +1,13 @@
-n = int(input())
+import sys
 
-a = 1
-b = 2
+N = int(sys.stdin.readline().rstrip())
 
-if n == 1:
-    print(1)
-elif n == 2:
-    print(2)
-else:    
-    for i in range(n-2):
-        c = a + b
-        a = b
-        b = c
-    
-    print(c%10007)
-        
+dp = [1, 2]
+def fib(n):
+    if n == 1 or n == 2:
+        return dp[n - 1]
+    for _ in range(2, N):
+        dp.append((dp[-1] + dp[-2]) % 10007)
+    return dp[-1]
+
+print(fib(N))
