96375be99b5bf1aab654d72754d1485f4ec08628
JiHwan Park <bat522@naver.com>
2023-04-17 19:47:30 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 31796 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -3,9 +3,9 @@ sys.setrecursionlimit(1000000)
 
 n = int(sys.stdin.readline())
 matrix = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
-visited = [[False] * n for _ in range(n)] # 방문 기록
+visited = [[False] * n for _ in range(n)]
 
-dx = [1, 0, -1, 0] # 방향을 나타냄
+dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
 cnt2, cnt3 = 0, 0
@@ -24,20 +24,20 @@ def dfs(x, y):
                     dfs(nx, ny)
 
 
-for i in range(n): # 적록색약이 아닌 사람
+for i in range(n):
     for j in range(n):
         if visited[i][j] == False:
             dfs(i, j)
             cnt3 += 1
 
-for i in range(n): # 적록색약으로 만들기
+for i in range(n):
     for j in range(n):
         if matrix[i][j] == 'R':
             matrix[i][j] = 'G'
 
 visited = [[False] * n for _ in range(n)] # 방문 기록 갱신
 
-for i in range(n): # 적록색약인 사람
+for i in range(n):
     for j in range(n):
         if visited[i][j] == False:
             dfs(i, j)
