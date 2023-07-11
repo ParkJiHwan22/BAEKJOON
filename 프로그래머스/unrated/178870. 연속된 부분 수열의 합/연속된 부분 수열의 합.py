2c24ccd6a35846f5b339599cb681e33cf25ad9a5
JiHwan Park <bat522@naver.com>
2023-07-11 09:21:38 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,40 +1,27 @@
 def solution(sequence, k):
-    s = 0
-    e = 0
-    total = 0
-    min_num = 1000001
-    answer = []
+    s = 0 # start
+    e = 0 # end
+    total = 0 # 총합
+    min_length = 1000001 # sequence의 길이의 최댓값 +1
+    answer = [] # 결과
 
-    while e <= len(sequence) and s < len(sequence):
-        if e < len(sequence):
-            if total < k:
+    while  s < len(sequence) and e <= len(sequence):
+        if total < k: # total < k 일 때, e를 증가시킴, 범위 확대(total 증가)
+            if e != len(sequence):          
                 total += sequence[e]
-                e += 1
+            e += 1
 
-            elif total == k:
-                if min_num > e - s:
-                    min_num = e - s
-                    answer = [s, e-1]
+        elif total == k: # total == k 일 때, e를 증가시킴, 범위 확대(total 증가)
+            if min_length > e - s: # 길이가 짧은 수열을 찾기
+                min_length = e - s # 가장 짧을 경우 min_length에 저장하고 
+                answer = [s, e-1] # res에 [s, e-1] 저장
 
+            if e != len(sequence):          
                 total += sequence[e]
-                e += 1
+            e += 1
 
-            else:
-                total -= sequence[s]
-                s += 1
-
-        else:
-            if total < k:
-                e += 1
-
-            elif total == k:
-                if min_num > e - s:
-                    min_num = e - s
-                    answer = [s, e-1]
-                e += 1
-
-            else:
-                total -= sequence[s]
-                s += 1
+        else: # total > k 일 때, s를 증가시킴, 범위 축소(total 감소)
+            total -= sequence[s]
+            s += 1
 
     return answer
