7868f3f99a0208e372c83271ed27e03d4dc3bbf4
JiHwan Park <bat522@naver.com>
2023-08-14 16:58:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,19 +1,13 @@
 import sys
 
-n = int(sys.stdin.readline())
-square = [sys.stdin.readline() for _ in range(n)]
-
-square_list = [[] for _ in range(n)]
-total = cnt = 0
+n = int(input())
+square_list = [list(map(int, input().rstrip())) for _ in range(n)]
+total = 0
+cnt = 0
 cnt_list = []
 
 dy = [0, -1, 0, 1]
 dx = [1, 0, -1, 0]
-
-for k in range(n):
-    for l in range(n):
-        square_list[k].append(int(square[k][l]))
-
 
 for i in range(n):
     for j in range(n):
@@ -33,7 +27,7 @@ for i in range(n):
                     if 0 <= y + dy[dir] < n and 0 <= x + dx[dir] < n:
                         if square_list[y + dy[dir]][x + dx[dir]] == 1:
                             cnt += 1
-                            square_list[y + dy[dir]][x + dx[dir]] = 2
+                            square_list[y + dy[dir]][x + dx[dir]] = 2 # 단지 별로 체크해서 lst에 저장
                             stack.append(x + dx[dir])
                             stack.append(y + dy[dir])
 
