9f8b19a2269eedeb06edbfbbc8c2abd78df8bb11
JiHwan Park <bat522@naver.com>
2023-08-21 13:49:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 672 ms, Memory: 65048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,25 +2,33 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n+1)]
-visited = [0]*(n + 1)
-answer = 0
+visited = [0 for _ in range(n+1)]
+
 
 for i in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a) 
-
-for i in range(1, n+1): # dfs
-    if visited[i] == 0: 
-        visited[i] = 1
-        answer += 1
-        stack = [i]
+    
+def dfs(n, graph, visited):
+    answer = 0
+    for i in range(1, n+1):
+        if visited[i] == 1:
+            continue
         
-        while stack:
-            p = stack.pop()
-            for j in graph[p]:
-                if visited[j] == 0:
-                    visited[j] = 1
-                    stack.append(j)
+        else:
+            answer += 1
+            visited[i] = 1
+            stack = [i]
+            
+            while stack:
+                p = stack.pop()
+                if graph[p]:
+                    for j in range(len(graph[p])):
+                        if visited[graph[p][j]] == 0:
+                            visited[graph[p][j]] = 1
+                            stack.append(graph[p][j])
+     
+    return answer
 
-print(answer)
+print(dfs(n, graph, visited))
