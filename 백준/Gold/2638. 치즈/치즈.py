5c67cc0f8827e5b51b172e91549ab1d37ba5ecd0
JiHwan Park <bat522@naver.com>
2023-08-22 10:09:34 +0900

    [Gold III] Title: 치즈, Time: 3036 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -4,21 +4,21 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 cheese = [list(map(int, input().split())) for _ in range(n)]
-day_cnt = 0
+cnt = 0
 res = 0
 
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
 while True:
-    day_bef = day_cnt
-    day_cnt = day_cnt + 2
+    cnt_before = cnt
+    cnt = cnt + 2
 
     for i in range(n):
         for j in range(m):
             if i == 0 or i == n - 1 or j == 0 or j == m - 1: # 겉쪽 치즈와 안쪽 치즈를 구분
-                if cheese[i][j] == 0 or cheese[i][j] == day_bef:
-                    cheese[i][j] = day_cnt
+                if cheese[i][j] == 0 or cheese[i][j] == cnt_before:
+                    cheese[i][j] = cnt
                     queue = deque([(j, i)])
                     
                 while queue:
@@ -29,14 +29,14 @@ while True:
                         nx = x + dx[t]
 
                         if 0 <= ny < n and 0 <= nx < m:
-                            if cheese[ny][nx] == 0 or cheese[ny][nx] == day_bef:
-                                cheese[ny][nx] = day_cnt
+                            if cheese[ny][nx] == 0 or cheese[ny][nx] == cnt_before:
+                                cheese[ny][nx] = cnt
                                 queue.append((nx, ny))
 
 
     for q in range(n): # 공기에 연결된 내부 공간
         for w in range(m):
-            if cheese[q][w] == day_cnt:
+            if cheese[q][w] == cnt:
                 queue_3 = deque([(w, q)])
                 
             while queue_3:
@@ -48,7 +48,7 @@ while True:
 
                     if 0 <= ny < n and 0 <= nx < m:
                         if cheese[ny][nx] == 0:
-                            cheese[ny][nx] = day_cnt
+                            cheese[ny][nx] = cnt
                             queue_3.append((nx, ny))                
 
 
@@ -61,18 +61,18 @@ while True:
                 
                 while queue_2:
                     x, y = queue_2.popleft()
-                    cnt = 0
+                    cheese_cnt = 0
                     
                     for t in range(4):
                         ny = y + dy[t]
                         nx = x + dx[t]
 
                         if 0 <= ny < n and 0 <= nx < m:
-                            if cheese[ny][nx] == day_cnt:
-                                cnt += 1
+                            if cheese[ny][nx] == cnt:
+                                cheese_cnt += 1
                                 
-                            if cnt == 2:
-                                cheese[y][x] = day_cnt + 2
+                            if cheese_cnt == 2:
+                                cheese[y][x] = cnt + 2
     
     if work == 0: # 사라지는 치즈가 없을 경우
         break
