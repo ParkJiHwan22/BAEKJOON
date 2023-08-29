feab25e074e1879822f4f25edb215f5f62e64803
JiHwan Park <bat522@naver.com>
2023-08-29 11:34:22 +0900

    [Gold III] Title: 아기 상어, Time: 184 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -5,48 +5,54 @@ sea = [list(map(int, input().split())) for _ in range(n)]
 level = 2
 fish_eaten = 0
 time = 0
+before_time = 0
 
-# Directions: up, down, left, right
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# Find the baby shark's position
+# 아기 상어 위치 찾기
 for i in range(n):
     for j in range(n):
         if sea[i][j] == 9:
             start_x, start_y = j, i
-            sea[i][j] = 0  # Set baby shark's position to empty
+            sea[i][j] = 0  # 아기 상어 위치를 0으로 변경
 
 while True:
     queue = deque([(start_x, start_y, 0)])
-    visited = [[False] * n for _ in range(n)]
-    visited[start_y][start_x] = True
-    fishes = []  # List of fishes that can be eaten
+    visited = [[0] * n for _ in range(n)]
+    visited[start_y][start_x] = 1
+    fishes = []  # 잡아먹힐 물고기들
 
     while queue:
         x, y, t = queue.popleft()
 
+        if fishes and before_time != time:
+            before_time = time
+            break
+            
         for dir in range(4):
             new_x = x + dx[dir]
             new_y = y + dy[dir]
 
-            if 0 <= new_x < n and 0 <= new_y < n and not visited[new_y][new_x]:
+            if 0 <= new_x < n and 0 <= new_y < n and not visited[new_y][new_x]: # 방문 여부 체크
                 visited[new_y][new_x] = True
                 
-                if sea[new_y][new_x] == 0 or sea[new_y][new_x] == level:
+                if sea[new_y][new_x] == 0 or sea[new_y][new_x] == level: # 먹을 수 없으나 지나가는 경우
                     queue.append((new_x, new_y, t + 1))
-                elif 0 < sea[new_y][new_x] < level:
+                elif 0 < sea[new_y][new_x] < level: # 먹을 수 있는 경우
                     fishes.append((new_x, new_y, t + 1))
+                    
+        before_time = time
 
-    # If no fish can be eaten, break
+    # 잡아먹힐 물고기가 없다면 식을 종료
     if not fishes:
         break
 
-    # Sort the fishes by distance, row, and column
-    fishes.sort(key=lambda x: (x[2], x[1], x[0]))
-    fish = fishes[0]
-    sea[fish[1]][fish[0]] = 0  # Eat the fish
-
+    # 물고기를 거리, 행, 열 순으로 정렬
+    fishes = sorted(fishes, key=lambda x: (x[2], x[1], x[0]))
+    fish = fishes[0] # 거리가 가장 가깝고 행, 열 기준으로 가장 위에 있는 물고기
+    sea[fish[1]][fish[0]] = 0  # 결국 잡아먹힌 물고기
+    
     fish_eaten += 1
     if fish_eaten == level:
         level += 1
