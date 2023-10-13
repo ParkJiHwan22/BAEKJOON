c5bcff3708f82e9f9202522d82b19b5812b3db73
JiHwan Park <bat522@naver.com>
2023-10-13 22:31:23 +0900

    [Silver V] Title: 3의 배수, Time: 5340 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -0,0 +1,18 @@
+n = input()
+cnt = 0
+
+while int(n) >= 10:
+    tmp = 0
+    for i in n:
+        tmp += int(i)
+        n = str(tmp)
+
+    cnt += 1
+
+print(cnt)
+
+if int(n) in [3, 6, 9]:
+    print('YES')
+
+else:
+    print('NO')
