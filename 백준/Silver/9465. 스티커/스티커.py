6f825e46e3df7c54e9df77649535415d3776f883
JiHwan Park <bat522@naver.com>
2023-11-04 19:30:09 +0900

    [Silver I] Title: 스티커, Time: 768 ms, Memory: 47740 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -5,12 +5,14 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    sticker = [list(map(int, input().split())) for _ in range(2)]
-    dp = [[0, 0, 0] for _ in range(n+1)]
+    dp = [list(map(int,input().split())) for _ in range(2)] # dp에 스티커값을 직접 입력받음
+    
+    if n > 1 : # n == 1 일 경우 dp에는 인덱스 0밖에 존재하지 않으므로 오류가 난다.
+        dp[0][1] += dp[1][0]
+        dp[1][1] += dp[0][0]
+            
+    for i in range(2, n): # dp[0][i-1]의 값과 dp[1][i-1]의 값을 서로 비교
+        dp[0][i] += max(dp[1][i-1], dp[1][i-2])
+        dp[1][i] += max(dp[0][i-1], dp[0][i-2])
 
-    for i in range(n):
-        dp[i+1][0] = max(dp[i][1] + sticker[0][i], dp[i][2] + sticker[0][i])
-        dp[i+1][1] = max(dp[i][0] + sticker[1][i], dp[i][2] + sticker[1][i])
-        dp[i+1][2] = max(dp[i])
-
-    print(max(dp[-1]))
+    print(max(dp[0][n-1], dp[1][n-1]))
