d726be57d2276cd241303326613f0f58257c9667
JiHwan Park <bat522@naver.com>
2023-11-06 12:39:20 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 120 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,22 +1,31 @@
 from collections import deque
 
-n, k = map(int, input().split())  # n: 수빈이가 있는 위치, k: 동생이 있는 위치
-q = deque()
-q.append(n) 
-visited = [-1 for _ in range(100001)]
-visited[n] = 0
+n, k = map(int, input().split())
 
-while q:
-    s = q.popleft()
-    if s == k:
-        print(visited[s])
-        break
-    if 0 <= s-1 < 100001 and visited[s-1] == -1:
-        visited[s-1] = visited[s] + 1
-        q.append(s-1)
-    if 0 < s*2 < 100001 and visited[s*2] == -1:
-        visited[s*2] = visited[s]
-        q.appendleft(s*2)  # 2*s 가 다른 연산보다 더 높은 우선순위를 가지기 위함
-    if 0 <= s+1 < 100001 and visited[s+1] == -1:
-        visited[s+1] = visited[s] + 1
-        q.append(s+1)
+if n >= k: # 수빈이가 동생 앞에 있을 떄
+    print(n - k)
+
+else:
+    MAX = 100001
+    q = deque()
+    q.append(n)
+    dp = [-1 for _ in range(MAX)]
+    dp[n] = 0
+
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(dp[x])
+            break
+        
+        if 0 <= x - 1 < MAX and dp[x - 1] == -1:
+            dp[x - 1] = dp[x] + 1
+            q.append(x - 1)
+            
+        if 0 < x * 2 < MAX and dp[x * 2] == -1:
+            dp[x * 2] = dp[x]            
+            q.appendleft(x * 2)
+            
+        if 0 <= x + 1 < MAX and dp[x + 1] == -1:
+            dp[x + 1] = dp[x] + 1
+            q.append(x + 1)
