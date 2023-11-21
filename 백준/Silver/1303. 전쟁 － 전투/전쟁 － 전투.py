ca37e6ff2b1c2bcad0d280e9149be79c65b7a35f
JiHwan Park <bat522@naver.com>
2023-11-21 16:51:28 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 80 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -6,53 +6,38 @@ n, m = map(int, input().split())
 
 soldier = [input().rstrip() for _ in range(m)]
 visited = [[0] * n for _ in range(m)]
-W_ans = []
-B_ans = []
-
+ans = [0, 0]
+q = deque()
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
-q = deque()
+def sol(x, y, color):
+    q.append((y, x))
+    tmp = 1
+    while q:
+        y, x = q.popleft()
+        
+        for dir in range(4):
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+            
+            if 0 <= nx < n and 0 <= ny < m and visited[ny][nx] == 0 and soldier[ny][nx] == color:
+                visited[ny][nx] = 1
+                tmp += 1
+                q.append((ny, nx))
 
+    if color == 'W':
+        ans[0] += tmp ** 2
+    else:
+        ans[1] += tmp ** 2
+        
 for i in range(m):
     for j in range(n):
         if visited[i][j] == 0:
+            visited[i][j] = 1
             if soldier[i][j] == 'W':
-                visited[i][j] = 1
-                q.append((i, j))
-
-                tmp = 1
-                while q:
-                    y, x = q.popleft()
-                    
-                    for dir in range(4):
-                        nx = x + dx[dir]
-                        ny = y + dy[dir]
-                        
-                        if 0 <= nx < n and 0 <= ny < m and visited[ny][nx] == 0 and soldier[ny][nx] == 'W':
-                            visited[ny][nx] = 1
-                            tmp += 1
-                            q.append((ny, nx))
-                
-                W_ans.append(tmp)
-                
+                sol(j, i, 'W')
             else:
-                visited[i][j] = 1
-                q.append((i, j))
+                sol(j, i, 'B')
 
-                tmp = 1
-                while q:
-                    y, x = q.popleft()
-                    
-                    for dir in range(4):
-                        nx = x + dx[dir]
-                        ny = y + dy[dir]
-                        
-                        if 0 <= nx < n and 0 <= ny < m and visited[ny][nx] == 0 and soldier[ny][nx] == 'B':
-                            visited[ny][nx] = 1
-                            tmp += 1
-                            q.append((ny, nx))
-                
-                B_ans.append(tmp)
-                
-print(sum([x ** 2 for x in W_ans]), (sum([y ** 2 for y in B_ans])))
+print(*ans)
