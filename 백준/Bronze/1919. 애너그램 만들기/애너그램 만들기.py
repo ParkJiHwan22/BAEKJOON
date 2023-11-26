b182bc77100f15606b89735c5925e9d5dd1fab23
JiHwan Park <bat522@naver.com>
2023-11-26 11:49:22 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+a_words = input().rstrip()
+b_words = input().rstrip()
+
+a_split = [i for i in a_words]
+b_split = [j for j in b_words]
+cnt = 0
+
+while a_split and b_split:
+    a = a_split.pop()
+    for b in range(len(b_split)):
+        if a == b_split[b]:
+            del b_split[b]
+            cnt += 2
+            break
+
+print(len(a_words) + len(b_words) - cnt)
