08aae25e3a48bef90c39a7e65a686da1ba61d288
JiHwan Park <bat522@naver.com>
2024-01-05 23:58:59 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] inputs = br.readLine().split(" ");
+        int A = Integer.parseInt(inputs[0]);
+        int B = Integer.parseInt(inputs[1]);
 
         System.out.println(A + B);
 
-        sc.close();
+        br.close();
     }
 }
