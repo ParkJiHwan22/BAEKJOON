e6520796ce5614ec65a9f9eac81a0fd1061de1f8
JiHwan Park <bat522@naver.com>
2024-04-23 17:33:22 +0900

    [Bronze V] Title: 초코바, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
+        
+        if (N * 100 / M >= 1) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
