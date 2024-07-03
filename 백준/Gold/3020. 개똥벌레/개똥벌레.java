b5c3d083a7765081ffd9eaad2fa8ec4a3c5e6e63
JiHwan Park <bat522@naver.com>
2024-07-03 14:08:19 +0900

    [Gold V] Title: 개똥벌레, Time: 340 ms, Memory: 32764 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        int[] odd = new int[H+1];
+        int[] even = new int[H+1];
+
+        for (int c = 1; c < N+1; c++) {
+            int curr = Integer.parseInt(br.readLine());
+            if(c % 2 != 0) odd[curr] += 1;
+            else even[curr] += 1;
+        }
+
+        for (int i = 1; i < H+1; i++) {
+            odd[i] = odd[i-1] + odd[i];
+            even[i] = even[i-1] + even[i];
+        }
+
+        int result2 = 0;
+
+        int[] list = new int[H+1];
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i < H+1; i++) {
+            list[i] += odd[H] - odd[i-1];
+            list[i] += even[H] - even[H-i];
+            min = Math.min(min, list[i]);
+        }
+
+        for (int i = 1; i < H+1; i++) {
+            if(list[i] == min) result2++;
+        }
+
+        System.out.println(min + " " + result2);
+    }
+}
