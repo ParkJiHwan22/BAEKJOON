e8053fb56d570415a19cfab23a67ba50446c67a8
JiHwan Park <bat522@naver.com>
2024-07-03 14:16:55 +0900

    [Gold V] Title: 개똥벌레, Time: 284 ms, Memory: 32976 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -12,32 +12,35 @@ public class Main {
         int[] odd = new int[H+1];
         int[] even = new int[H+1];
 
+        // 입력
         for (int c = 1; c < N+1; c++) {
             int curr = Integer.parseInt(br.readLine());
             if(c % 2 != 0) odd[curr] += 1;
             else even[curr] += 1;
         }
-
+        
+        // 누적합
         for (int i = 1; i < H+1; i++) {
             odd[i] = odd[i-1] + odd[i];
             even[i] = even[i-1] + even[i];
         }
 
-        int result2 = 0;
+        int res = 0;
 
         int[] list = new int[H+1];
         int min = Integer.MAX_VALUE;
 
+        // 아이디어
         for (int i = 1; i < H+1; i++) {
             list[i] += odd[H] - odd[i-1];
             list[i] += even[H] - even[H-i];
             min = Math.min(min, list[i]);
         }
-
+        
         for (int i = 1; i < H+1; i++) {
-            if(list[i] == min) result2++;
+            if(list[i] == min) res++;
         }
 
-        System.out.println(min + " " + result2);
+        System.out.println(min + " " + res);
     }
 }
