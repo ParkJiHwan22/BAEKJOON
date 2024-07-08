39a74ed23685f15d9e97950873890aa5850a2270
JiHwan Park <bat522@naver.com>
2024-07-08 21:06:21 +0900

    [Silver III] Title: 합 구하기, Time: 632 ms, Memory: 52408 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.java
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -2,23 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] cul = new int[N+1];
+    public static void main(String[] args) throws IOException {
+        // BufferedReader와 BufferedWriter를 사용하여 입력과 출력을 최적화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] cul = new int[N + 1];
 
-		for (int i = 0; i < N; i ++) {
-			int k = Integer.parseInt(st.nextToken());
-			cul[i+1] += k + cul[i];
-		}
-		int M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < M; i ++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			System.out.println(cul[b] - cul[a-1]);
-		}
-	}
+        for (int i = 0; i < N; i++) {
+            int k = Integer.parseInt(st.nextToken());
+            cul[i + 1] = cul[i] + k;
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            // BufferedWriter를 사용하여 출력
+            bw.write((cul[b] - cul[a - 1]) + "\n");
+        }
+        
+        // BufferedWriter를 닫아 출력 내용을 최종적으로 flush
+        bw.flush();
+        bw.close();
+    }
 }
