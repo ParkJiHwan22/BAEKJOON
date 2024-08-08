4501dc5d066373f9322c7ac10774ed733153ecd4
JiHwan Park <bat522@naver.com>
2024-08-08 21:06:26 +0900

    [Gold IV] Title: 여행 가자, Time: 148 ms, Memory: 16992 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,53 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		
-		List<List<Integer>> arr = new ArrayList<List<Integer>>();
-		int[] plan = new int[M];
-		boolean[] visited = new boolean[N+1];
-		
-		for (int i = 0; i <= N; i++) {
-			arr.add(new ArrayList<Integer>());
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				int k = Integer.parseInt(st.nextToken());
-				if (k == 1) arr.get(i).add(j);
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			int k = Integer.parseInt(st.nextToken());
-			plan[i] = k;
-		}
-		
-		Queue<Integer> q = new LinkedList<>();
-		q.add(plan[0]);
-		visited[plan[0]] = true;
-		while (!q.isEmpty()) {
-			int tmp = q.poll();
-			for (int k : arr.get(tmp)) {
-				if (!visited[k]) { // 방문하지 않았을 경우
-					visited[k] = true;
-					q.add(k);
-				}
-			}
-		}
-		
-		for (int k : plan) {
-			if (!visited[k]) {
-				System.out.println("NO");
-				return;
-			}
-		}
-		System.out.println("YES");
-	}
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int isConnected = Integer.parseInt(st.nextToken());
+                if (isConnected == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int[] plan = new int[M];
+        for (int i = 0; i < M; i++) {
+            plan[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean isPossible = true;
+        for (int i = 1; i < M; i++) {
+            if (find(plan[i]) != find(plan[i - 1])) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
 }
