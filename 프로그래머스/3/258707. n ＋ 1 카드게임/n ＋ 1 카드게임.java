587dda0f0533aa6d2f7cd1774380e2fc0261ce91
JiHwan Park <bat522@naver.com>
2024-08-24 00:29:06 +0900

    [level 3] Title: n + 1 카드게임, Time: 18.45 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,101 +1,73 @@
 import java.util.*;
-
 class Solution {
     public int solution(int coin, int[] cards) {
-        int answer = 0;
-        int life = 0;
-        int moreCoin = 0;
+        // 처음 set과 추가하려고 하는 addset
+        Set<Integer> originalSet = new HashSet();
+        Set<Integer> addSet = new HashSet();
+        int result = 0;
+        int goal = cards.length + 1;
+        int cardsCnt = cards.length / 3;
         
-        List<Integer> save = new ArrayList<>();
-        List<Integer> tmp = new ArrayList<>();
-
-        for (int i = 0; i < cards.length / 3; i++) {
-            save.add(cards[i]);
-            for (int j = 0; j < save.size() - 1; j++) {
-                if (save.get(j) == cards.length + 1 - save.get(save.size() - 1)) {
-                    save.remove(save.size() - 1);
-                    save.remove(j);
-                    life++;
-                    break;
-                }
-            }
+        for(int i = 0; i < cardsCnt; i++) {
+            originalSet.add(cards[i]);
         }
         
-        // 카드를 2장 뽑고 
-        // y, y: save에 있으면 숫자를 뽑음 (2번 반복)
-        // y, n: save에 있으면 숫자를 뽑고, save에 넣어둠
-        // n, y: tmp에 넣어두고, save에 있으면 숫자를 뽑고 진행
-        // n, n: 둘다 tmp에 넣어두고 tmp 마지막 숫자와 겹치는게 있는지 진행
-        // tmp는 코인을 안쓰고 저장해두는 용도
-        
-        // 매 과정 끝마다 tmp 맨뒤 숫자와 확인하는 로직 추가
-        // 카드를 버릴 때마다 life ++;
-        
-        // 카드를 넣을지 뺄지 우선순위 설정하는 게 없음
-        
-        // 1번
-        for (int i = cards.length / 3; i < cards.length; i+= 2) {
-            boolean is_saved = false;
-            for (int j = 0; j < save.size(); j++) {
-                if (save.get(j) == cards.length + 1 - cards[i] && coin > 0) {
-                    save.remove(j);
-                    life++;
-                    coin--;
-                    is_saved = true;
+        while(true) {
+            result++;
+            
+            if(cardsCnt >= cards.length) break;
+            
+            addSet.add(cards[cardsCnt]);
+            addSet.add(cards[cardsCnt + 1]);
+            cardsCnt += 2;
+            
+            // 있는 카드로 해결 가능할 때
+            boolean flag = false;
+            for(int i : originalSet) {
+                if(originalSet.contains(goal - i)) {
+                    originalSet.remove(goal - i);
+                    addSet.remove(i);
+                    flag = true;
                     break;
                 }
             }
             
-            if (!is_saved) {
-                tmp.add(cards[i]);
-                for (int j = 0; j < tmp.size() - 1; j++) {
-                    if (tmp.get(j) == cards.length + 1 - tmp.get(tmp.size() - 1)) {
-                        tmp.remove(tmp.size() - 1);
-                        tmp.remove(tmp.get(j));
-                        moreCoin++;
-                        break;
+            // 카드 한 장 뽑아서 해결 가능할 때
+            if(!flag) {
+                if(coin > 0) {
+                    for(int i : originalSet) {
+                        if(addSet.contains(goal - i)) {
+                            addSet.remove(goal - i);
+                            originalSet.remove(i);
+                            coin--;
+                            flag = true;
+                            break;
+                        }           
                     }
                 }
             }
             
-            // 2번
-            is_saved = false;
-            for (int j = 0; j < save.size(); j++) {
-                if (save.get(j) == cards.length + 1 - cards[i+1] && coin > 0) {
-                    save.remove(j);
-                    life++;
-                    coin--;
-                    is_saved = true;
-                    break;
-                }
-            }
-            
-            if (!is_saved) {
-                tmp.add(cards[i+1]);
-                for (int j = 0; j < tmp.size() - 1; j++) {
-                    if (tmp.get(j) == cards.length + 1 - tmp.get(tmp.size() - 1)) {
-                        tmp.remove(tmp.size() - 1);
-                        tmp.remove(tmp.get(j));
-                        moreCoin++;
-                        break;
+            // 카드 두 장 뽑아서 해결 가능할 때
+            if(!flag) {
+                if(coin > 1) {
+                    for(int i : addSet) {
+                        if(addSet.contains(goal - i)) {
+                            addSet.remove(i);
+                            addSet.remove(goal - i);
+                            coin -= 2;
+                            flag = true;
+                            break;
+                        }
                     }
                 }
             }
             
-            if (life == 0) {
-                if (coin >= 2 && moreCoin > 0) {
-                    coin -= 2;
-                    moreCoin --;
-                    answer++;
-                } else {
-                    break;
-                }
-            } else {
-                life --;
-                answer++;
-            }
+            // 해결 가능하지 않을 때
+            if(!flag) break;
+            
         }
         
-        return answer + 1;
+        return result;
+        
     }
 }
