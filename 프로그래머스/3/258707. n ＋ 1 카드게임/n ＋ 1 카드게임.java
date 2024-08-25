074a9ff03ad67a14ba761b385e19b41af908f213
JiHwan Park <bat522@naver.com>
2024-08-25 14:56:14 +0900

    [level 3] Title: n + 1 카드게임, Time: 13.32 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,73 +1,66 @@
 import java.util.*;
+
 class Solution {
     public int solution(int coin, int[] cards) {
-        // 처음 set과 추가하려고 하는 addset
-        Set<Integer> originalSet = new HashSet();
-        Set<Integer> addSet = new HashSet();
-        int result = 0;
-        int goal = cards.length + 1;
-        int cardsCnt = cards.length / 3;
-        
-        for(int i = 0; i < cardsCnt; i++) {
-            originalSet.add(cards[i]);
-        }
-        
-        while(true) {
-            result++;
-            
-            if(cardsCnt >= cards.length) break;
-            
-            addSet.add(cards[cardsCnt]);
-            addSet.add(cards[cardsCnt + 1]);
-            cardsCnt += 2;
-            
-            // 있는 카드로 해결 가능할 때
-            boolean flag = false;
-            for(int i : originalSet) {
-                if(originalSet.contains(goal - i)) {
-                    originalSet.remove(goal - i);
-                    addSet.remove(i);
-                    flag = true;
+        int answer = 0;
+        int life = 0;
+        int moreCoin = 0;
+
+        List<Integer> save = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+
+        for (int i = 0; i < cards.length / 3; i++) {
+            save.add(cards[i]);
+            for (int j = 0; j < save.size() - 1; j++) {
+                if (save.get(j) == cards.length + 1 - save.get(save.size() - 1)) {
+                    save.remove(save.size() - 1);
+                    save.remove(j);
+                    life++;
                     break;
                 }
             }
-            
-            // 카드 한 장 뽑아서 해결 가능할 때
-            if(!flag) {
-                if(coin > 0) {
-                    for(int i : originalSet) {
-                        if(addSet.contains(goal - i)) {
-                            addSet.remove(goal - i);
-                            originalSet.remove(i);
-                            coin--;
-                            flag = true;
-                            break;
-                        }           
+        }
+        
+        for (int i = cards.length / 3; i < cards.length; i+= 2) {
+            for (int k = 0; k < 2; k++) {
+                boolean is_saved = false;
+                for (int j = 0; j < save.size(); j++) {
+                    if (save.get(j) == cards.length + 1 - cards[i + k] && coin > 0) {
+                        save.remove(j);
+                        life++;
+                        coin--;
+                        is_saved = true;
+                        break;
                     }
                 }
-            }
-            
-            // 카드 두 장 뽑아서 해결 가능할 때
-            if(!flag) {
-                if(coin > 1) {
-                    for(int i : addSet) {
-                        if(addSet.contains(goal - i)) {
-                            addSet.remove(i);
-                            addSet.remove(goal - i);
-                            coin -= 2;
-                            flag = true;
+
+                if (!is_saved) {
+                    tmp.add(cards[i + k]);
+                    for (int j = 0; j < tmp.size() - 1; j++) {
+                        if (tmp.get(j) == cards.length + 1 - tmp.get(tmp.size() - 1)) {
+                            tmp.remove(tmp.size() - 1);
+                            tmp.remove(tmp.get(j));
+                            moreCoin++;
                             break;
                         }
                     }
                 }
             }
-            
-            // 해결 가능하지 않을 때
-            if(!flag) break;
-            
+
+            if (life == 0) {
+                if (coin >= 2 && moreCoin > 0) {
+                    coin -= 2;
+                    moreCoin --;
+                    answer++;
+                } else {
+                    break;
+                }
+            } else {
+                life --;
+                answer++;
+            }
         }
-        
-        return result;
-        
+
+        return answer + 1;
     }
 }
