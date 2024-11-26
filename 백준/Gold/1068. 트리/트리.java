30f7bcb16959d3dbbdd32a46662d52fd1ab3e79d
JiHwan Park <bat522@naver.com>
2024-11-26 14:42:36 +0900

    [Gold V] Title: 트리, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,44 +1,57 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] son = new int[N];
-		int[] arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			if (arr[i] != -1) son[arr[i]] ++;
-		}
-		int D = Integer.parseInt(br.readLine());
-				
-		Queue<Integer> q = new LinkedList<>();
-		q.add(D);
-		
-		boolean[] visited = new boolean[N];
-		
-		while (!q.isEmpty()) {
-			int p = q.poll();
-			son[p] = -1;
-			if (arr[p] != -1) son[arr[p]]--;
-			for (int i = 0; i < N; i++) {
-				if (arr[i] == p && !visited[i]) {
-					visited[i] = true;
-					q.add(i);
-				}
-			}
-		}
-		
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			if (son[i] == 0) cnt++;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
 
-		System.out.println(cnt);
-	}
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            arr.add(new ArrayList<>());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr.get(i).add(i);
+            int num = Integer.parseInt(st.nextToken());
+            if (num >= 0) arr.get(num).add(i);
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        q.add(t);
+
+        while (!q.isEmpty()) {
+            int tmp = q.poll();
+
+            // 위쪽에 대한 처리
+            for (int i = 0; i < tmp; i++) {
+                for (int j = 0; j < arr.get(i).size(); j++) {
+                    if (arr.get(i).get(j) == tmp) {
+                        arr.get(i).remove(j);
+                    }
+                }
+            }
+
+
+            // 아래쪽에 대한 처리
+            for (int arrNum : arr.get(tmp)) {
+                q.add(arrNum);
+            }
+            arr.get(tmp).clear();
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (arr.get(i).size() == 1) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
 }
