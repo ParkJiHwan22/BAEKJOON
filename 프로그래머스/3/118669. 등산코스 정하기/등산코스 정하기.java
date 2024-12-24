dd8f045f969a75cfd87992ff6642ea5eea04af82
JiHwan Park <bat522@naver.com>
2024-12-24 15:40:28 +0900

    [level 3] Title: 등산코스 정하기, Time: 245.20 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -4,57 +4,50 @@ class Solution {
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         int[] answer = new int[] {1000000000, 1000000000};
         int[] visited = new int[n+1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
+
+        // 시작점 기록
+        for (int i = 0; i < gates.length; i++) {
+            visited[gates[i]] = -1;
+        }
         
-        // paths의 원소는 [i, j, w] 형태
+        // 끝점 기록
+        for (int i = 0; i < summits.length; i++) {
+            visited[summits[i]] = -2;
+        }
+        
         List<List<int[]>> arr = new ArrayList<List<int[]>>();
-        for (int i = 0; i <= n; i++) {
+        
+        for (int i = 0; i < n+1; i++) {
             arr.add(new ArrayList<int[]>());
         }
-        
-        for (int[] path : paths) { // 저장할 때는 [end, d]
-            arr.get(path[0]).add(new int[] {path[1], path[2]});
-            arr.get(path[1]).add(new int[] {path[0], path[2]});
+
+        for (int i = 0; i < paths.length; i++) {
+            arr.get(paths[i][0]).add(new int[]{paths[i][1], paths[i][2]});
+            arr.get(paths[i][1]).add(new int[]{paths[i][0], paths[i][2]});
         }
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt((int[] a) -> a[0]));
-        for (int gate : gates) { // gate -> -1
-            visited[gate] = -1;
-            for (int[] start : arr.get(gate)) {
-                pq.add(new int[] {start[1], start[0], start[1]});
-            }
-        }
+        for (int i = 0; i < gates.length; i++) {
+            Queue<int[]> q = new LinkedList<>();
+            q.add(new int[] {gates[i], 0});
         
-        for (int summit : summits) { // gate -> -2
-            visited[summit] = -2;
-        }
-        
-        while (!pq.isEmpty()) {
-            int[] tmp = pq.poll(); //  [시작 값과 이동거리의 최댓값, 이동 거리, 도착 위치]
-            int sdMax = tmp[0];
-            int end = tmp[1];
-            int d = tmp[2];
-            
-            
-            if (visited[end] == 0) { // 1. 아직 방문하지 않았을 경우
-                visited[end] = Math.max(sdMax, d);
-                // answer[1] = Math.max(answer[1], visited[end]);
-                for (int[] val : arr.get(end)) {
-                    pq.add(new int[] {Math.max(visited[end], val[1]), val[0], val[1]});
-                }
-            } else if (visited[end] == -1) { // 2. 출발지인 경우
-                continue;
-            } else if (visited[end] == -2) { // 3. 도착지인 경우
-                if (sdMax < answer[1]) {
-                    answer[0] = end;
-                    answer[1] = sdMax;   
-                } else if (sdMax == answer[1] && answer[0] > end) {
-                    answer[0] = end;
-                }
-            } else if (sdMax < visited[end]) { // 4. 더 짧은 길이 있을 경우
-                visited[end] = Math.max(sdMax, d);
-                // answer[1] = Math.max(answer[1], visited[end]);
-                for (int[] val : arr.get(end)) {
-                    pq.add(new int[] {Math.max(visited[end], val[1]), val[0], val[1]});
+            while(!q.isEmpty()) {
+                int[] tmp = q.poll();
+                
+                for (int[] tmpNums : arr.get(tmp[0])) {
+                    if (visited[tmpNums[0]] == -1) { // 시작점일 경우
+                        continue;
+                    } else if (visited[tmpNums[0]] == -2) { // 끝점일 경우
+                        if (answer[1] > Math.max(tmp[1], tmpNums[1])) {
+                            answer[0] = tmpNums[0];
+                            answer[1] = Math.max(tmp[1], tmpNums[1]);
+                        } else if (answer[1] == Math.max(tmp[1], tmpNums[1]) && answer[0] > tmpNums[0]) {
+                            answer[0] = tmpNums[0];
+                        }
+                    } else if (visited[tmpNums[0]] > Math.max(tmp[1], tmpNums[1])) { // 쉼터일 경우
+                        visited[tmpNums[0]] = Math.max(tmp[1], tmpNums[1]);
+                        q.add(new int[]{tmpNums[0], visited[tmpNums[0]]});
+                    }
                 }
             }
         }
